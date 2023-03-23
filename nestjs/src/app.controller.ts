d0ebde124a0ebbac5a0b1e00b327997776b3fba7
Gaizgrol <gabrielizotongo@gmail.com>
2023-03-23 12:27:20 -0300

    feat: healthcheck and service dependencies

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @HttpCode(HttpStatus.OK)
+  @Get('/healthcheck')
+  healthCheck() {
+    return {
+      healthy: true,
+    };
   }
 }
